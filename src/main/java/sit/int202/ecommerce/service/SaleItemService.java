314b696939d68901a297e26fbd44aa41016590d7
Theerakan Choochat <tonpeech11@gmail.com>
2025-05-05 16:59:58 +0700

    [045] feat(sale-item-gallery): edit path again and import new model from db

--- src/main/java/sit/int202/ecommerce/service/SaleItemService.java
+++ src/main/java/sit/int202/ecommerce/service/SaleItemService.java
@@ -1,6 +1,5 @@
 package sit.int202.ecommerce.service;
 
-import jakarta.persistence.EntityNotFoundException;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 import sit.int202.ecommerce.dto.SaleItemGalleryResponse;
