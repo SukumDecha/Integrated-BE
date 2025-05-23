c61dd2d29def62c7d82e80585fcb811ee5b962be
Chicha Aksawasewe <142304426+Chichakub@users.noreply.github.com>
2025-05-23 22:15:55 +0700

    [024] feat(sale-item-pagination): implement v2 api with pagination, filtering, sorting, and swagger docs

--- src/main/java/sit/int202/ecommerce/repository/SaleItemRepository.java
+++ src/main/java/sit/int202/ecommerce/repository/SaleItemRepository.java
@@ -1,5 +1,7 @@
 package sit.int202.ecommerce.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import sit.int202.ecommerce.model.SaleItem;
 
@@ -7,5 +9,7 @@ import java.util.List;
 
 public interface SaleItemRepository extends JpaRepository<SaleItem, Integer> {
     List<SaleItem> findAllByOrderByCreatedOnAscIdAsc();
+    Page<SaleItem> findByBrand_NameIn(List<String> brandNames, Pageable pageable);
+
 
 }
