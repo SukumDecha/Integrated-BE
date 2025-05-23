747d489725a382d10c98a1477a5191c0d6a3bfef
Chicha Aksawasewe <142304426+Chichakub@users.noreply.github.com>
2025-05-23 23:31:48 +0700

    [024] feat(sale-item-pagination): reuse pagination response via paginationutil

--- src/main/java/sit/int202/ecommerce/util/PaginationUtil.java
+++ src/main/java/sit/int202/ecommerce/util/PaginationUtil.java
@@ -1,4 +1,21 @@
 package sit.int202.ecommerce.util;
 
+import org.springframework.data.domain.Page;
+import sit.int202.ecommerce.dto.response.PaginateResponse;
+
 public class PaginationUtil {
+
+    public static <T> PaginateResponse<T> toPaginateResponse(Page<T> page) {
+        PaginateResponse<T> response = new PaginateResponse<>();
+        response.setContent(page.getContent());
+        response.setFirst(page.isFirst());
+        response.setLast(page.isLast());
+        response.setPage(page.getNumber());
+        response.setSize(page.getSize());
+        response.setTotalPages(page.getTotalPages());
+        response.setTotalElements(page.getTotalElements());
+        response.setSort(page.getSort().toString());
+        return response;
+    }
+
 }
