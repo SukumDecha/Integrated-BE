915b12e997d8f333b9bc0f386f585e205199d779
SukumDecha <ninemaster12gt@gmail.com>
2025-10-27 00:00:52 +0700

    [038] refactor

--- src/main/java/sit/int202/ecommerce/modules/user/dto/request/UserLoginRequest.java
+++ src/main/java/sit/int202/ecommerce/modules/user/dto/request/UserLoginRequest.java
@@ -1,13 +1,16 @@
 package sit.int202.ecommerce.modules.user.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class UserLoginRequest {
     @Schema(description = "User email", example = "itbkk.somchai@ad.sit.kmutt.ac.th")
+    @NotBlank
     private String email;
 
     @Schema(description = "User password", example = "itProj24*SOM")
+    @NotBlank
     private String password;
 }
